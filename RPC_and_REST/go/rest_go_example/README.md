af2c12ebd3ffbc0aaf50f6ed1e6e171abe921860
stefanos1316 <stefanos1316@gmail.com>
2018-04-18 18:10:15 +0300

    Added simple hello world examples for go

---
+++ RPC_and_REST/go/rest_go_example/README.md
@@ -0,0 +1,3 @@
+# Executing 
+
+run it with go 1.9 version found in /usr/local/go/bin/go run main.go
---
+++ RPC_and_REST/go/rest_go_example/client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "net/http"
+    "fmt"
+    "log"
+    "io/ioutil"
+)
+
+func main() {
+
+    url := "http://localhost:3000"
+    req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+
+    //fmt.Printf("STATUS: ",resp.Status)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+}
