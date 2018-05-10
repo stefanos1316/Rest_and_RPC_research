47f548fa1a7e1c4da753bcf3468bb5bbbd41cce2
stefanos1316 <stefanos1316@gmail.com>
2018-05-10 18:00:31 +0300

    Fixed client scripts

--- tasks/javascript/rest/client.js
+++ tasks/javascript/rest/client.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
-for (var i = 0; i < 4500; i++) { 
-request('http://localhost:3000/', function (error, response, body) {
+for (var i = 0; i < 20000; i++) { 
+request('http://195.251.251.27:3000/', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log('STATUS: ' + response.statusCode) // Print the google web page.
      }
