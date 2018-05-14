2f63025f3c02842381ace5bd53eb5683288956dd
stefanos1316 <stefanos1316@gmail.com>
2018-05-14 18:22:57 +0300

    Added and modified rest client in jva

--- tasks/java/rest/REST_client/src/client.java
+++ tasks/java/rest/REST_client/src/client.java
@@ -7,15 +7,18 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-public class Test {
+public class client {
  public static void main(String[] args) throws ClientProtocolException, IOException {
   HttpClient client = new DefaultHttpClient();
-  HttpGet request = new HttpGet("http://195.251.251.27:8080/RESTfulExample/rest/hello/stefanos");
+  HttpGet request = new HttpGet("http://195.251.251.27:8080/RESTfulServer/rest/hello/stefanos");
+for (int i = 0; i < 20000; ++i) {
   HttpResponse response = client.execute(request);
   BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
   String line = "";
   while ((line = rd.readLine()) != null) {
-    System.out.println(line);
+  //  System.out.println(line);
   }
+}
+	System.out.println("Done");
  }
 }
