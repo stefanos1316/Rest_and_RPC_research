3e688b9187c7c46ade1c0d2f32df58de702eb178
stefanos1316 <stefanos1316@gmail.com>
2018-05-14 18:07:16 +0300

    Pushing correct client

---
+++ tasks/java/rest/REST_client/src/Test.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class Test {
+ public static void main(String[] args) throws ClientProtocolException, IOException {
+  HttpClient client = new DefaultHttpClient();
+  HttpGet request = new HttpGet("http://195.251.251.27:8080/RESTfulExample/rest/hello/stefanos");
+  HttpResponse response = client.execute(request);
+  BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+  String line = "";
+  while ((line = rd.readLine()) != null) {
+    System.out.println(line);
+  }
+ }
+}
