fad42566c7fb24bb752d827c73e247cd8fb47206
stefanos1316 <stefanos1316@gmail.com>
2018-05-17 14:55:00 +0300

    Prepared script for formatting data

---
+++ scripts/plotResults.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# This script's responsibility is of creating text files from the collect results in a graph plottable form
+
+if [ "$1" == "--help" ]; then
+	echo ""
+	echo "Usage of script.createPlottableData"
+	echo "==================================="
+	echo ""
+	echo "-p | --relatedPath	Provide the related path where all the Experimental Data are located."
+	echo ""
+	exit
+fi
+
+
+# Globals
+DIRECTORY_PATH=""
+
+
+OPTIONS=`getopt -o -p: --long --absoluteDataPath -n 'plotResults.sh' -- "$@"`
+eval set -- "$OPTIONS"
+while true; do
+	case "$1" in 
+		-p|--relatedPath) 
+			case $2 in 
+				*/*) DIRECTORY_PATH=$2 ; shift 2 ;;
+				*) >&2 echo "[Error] Directory path is required!" ; shift 2 ;;
+			esac ;;
+		--) shift ; break ;;
+		*) >&2 echo "Wrong command line argument, please try again." ; exit 1 ;;
+	esac
+done
+
+# Create directory where all the adjusted results will be stored
+if [ ! -d ${DIRECTORY_PATH}/graph_data ]; then
+	mkdir -p ${DIRECTORY_PATH}/graph_data
+else
+	rm -rf ${DIRECTORY_PATH}/graph_data/*
+fi
+
+# At this part we are going to interate through the directories that offers data for energy and performance for the server and client instance.
+for i in `ls ${DIRECTORY_PATH}`; do 
+	#echo "Directory name is $i"
+	for j in `ls ${DIRECTORY_PATH}/$i`; do
+		#echo "Programming language sequence is $j"
+		for k in `ls ${DIRECTORY_PATH}/$i/$j`; do
+			#echo "Protocol is $k"
+			
+			# Now we are going to read the files, and we are going to act respectivily
+			FILE=$(echo "$j.txt")
+			case $i in 
+				energy_results_server) 
+					TOTAL_CONSUMPTION=0
+
+					while IFS= read -r var; do 
+						TOTAL_CONSUMPTION=$(echo ${TOTAL_CONSUMPTION} + $var | bc)
+					done < "${DIRECTORY_PATH}/$i/$j/$k/${FILE}"
+					echo "Language:$k,Protocol:$j,Energy:${TOTAL_CONSUMPTION}" >> ${DIRECTORY_PATH}/graph_data/energy_server.txt
+					;;
+				energy_results_client) 
+					TOTAL_CONSUMPTION=0
+
+					while IFS= read -r var; do 
+						TOTAL_CONSUMPTION=$(echo ${TOTAL_CONSUMPTION} + $var | bc)
+					done < "${DIRECTORY_PATH}/$i/$j/$k/${FILE}"
+					echo "Language:$k,Protocol:$j,Energy:${TOTAL_CONSUMPTION}" >> ${DIRECTORY_PATH}/graph_data/energy_client.txt
+					;;
+				performance_results_server) 
+					MINUTES=$(cat ${DIRECTORY_PATH}/$i/$j/$k/${FILE}| grep "real" | awk '{print $2}' | awk -F "." '{print $1}' | awk -F "m" '{print $1}')
+					SECONDS=$(cat ${DIRECTORY_PATH}/$i/$j/$k/${FILE}| grep "real" | awk '{print $2}' | awk -F "." '{print $1}' | awk -F "m" '{print $2}')
+
+					if [ ${MINUTES} -ne 0 ]; then
+						MINUTES_TO_SECONDS=$((MINUTES * 60))
+						SECONDS=$((SECONDS + MINUTES_TO_SECONDS))
+					fi
+
+					echo "Language:$j,Protocol:$k,Time:${SECONDS}" >> ${DIRECTORY_PATH}/graph_data/performance_server.txt
+					;;
+				performance_results_client) 
+					FILE=$(echo "$j.txt")
+					MINUTES=$(cat ${DIRECTORY_PATH}/$i/$j/$k/${FILE}| grep "real" | awk '{print $2}' | awk -F "." '{print $1}' | awk -F "m" '{print $1}')
+					SECONDS=$(cat ${DIRECTORY_PATH}/$i/$j/$k/${FILE}| grep "real" | awk '{print $2}' | awk -F "." '{print $1}' | awk -F "m" '{print $2}')
+
+					if [ ${MINUTES} -ne 0 ]; then
+						MINUTES_TO_SECONDS=$((MINUTES * 60))
+						SECONDS=$((SECONDS + MINUTES_TO_SECONDS))
+					fi
+
+					echo "Language:$j,Protocol:$k,Time:${SECONDS}" >> ${DIRECTORY_PATH}/graph_data/performance_client.txt
+					;;
+			esac
+		done
+	done
+done
+
+echo Done, exiting...
+exit 1
+
