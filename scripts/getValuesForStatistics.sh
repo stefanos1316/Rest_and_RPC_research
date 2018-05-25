43f997e9a96355fb77b142f7a441aa1f55362319
stefanos1316 <stefanos1316@gmail.com>
2018-05-25 12:20:59 +0300

    Calculated SD for client's energy and performance

--- scripts/getValuesForStatistics.sh
+++ scripts/getValuesForStatistics.sh
@@ -9,8 +9,10 @@ PROTOCOLS_ARRAY=($PROTOCOLS)
 STRING=""
 
 echo
-echo "Performance measurements..."
+echo "[Client] Performance measurements..."
 
+rm ../statistics/client_performance_values.txt
+rm ../statistics/client_energy_values.txt
 for i in "${PROTOCOLS_ARRAY[@]}"; do
         for j in "${LANGUAGES_ARRAY[@]}"; do
                 for k in `ls $1`; do
@@ -25,10 +27,31 @@ for i in "${PROTOCOLS_ARRAY[@]}"; do
 				fi
 			STRING=${STRING}","${VALUE}			
 		done
- 		echo "Results for $j and $i is ${STRING}"
+ 		echo "Results for $j and $i is ${STRING}" >> ../statistics/client_performance_values.txt
 		STRING=""
  	done
 done
 
+echo
+echo "[Client] Energy measurements..."
+
+for i in "${PROTOCOLS_ARRAY[@]}"; do
+        for j in "${LANGUAGES_ARRAY[@]}"; do
+                for k in `ls $1`; do
+                        VALUE=""
+                        PATH_DATA=$(echo "$1/$k/graph_data/energy_client.txt")
+
+
+                                VALUE=$(cat ${PATH_DATA} | grep "$i" | grep "$j" | awk -F ":" '{print $4}')
+
+                                if [ -z "${VALUE}" ]; then
+                                        break
+				fi
+			STRING=${STRING}","${VALUE}			
+		done
+ 		echo "Results for $j and $i is ${STRING}" >> ../statistics/client_energy_values.txt
+		STRING=""
+ 	done
+done
 
 
