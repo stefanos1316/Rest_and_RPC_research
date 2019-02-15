00e560a32734432366f2508d2604aaf8150d9156
stefanos1316 <stefanos1316@gmail.com>
2019-02-15 15:52:09 +0200

    Added script to collect values from all executions

--- scripts/getValuesForStatistics.sh
+++ scripts/getValuesForStatistics.sh
@@ -1,105 +1,42 @@
 #!/bin/bash
 
+# The $1 can be a directory where all the execution results are located
+# such as experinemtn_***_/ where the graph_data directory is found to perform our analysis
 
-LANGUAGES="go java, javascript python"
-PROTOCOLS="rest :rpc grpc jax_ws_rpc"
+# The $2 is to which filename to write the data
 
-LANGUAGES_ARRAY=($LANGUAGES)
-PROTOCOLS_ARRAY=($PROTOCOLS)
-STRING=""
+pathToData=$2
+results=""
+languages="csharp	go	java	javascript	php	python	ruby"
+ipc="grpc rest rpc"
 
-rm ../statistics/client_performance_values.txt
-rm ../statistics/client_energy_values.txt
-rm ../statistics/server_performance_values.txt
-rm ../statistics/server_energy_values.txt
+if [ -f ${pathToData} ]; then
+   	echo "File already exist..."
+	echo Deleting
+	rm ${pathToData}
+fi
 
+echo ${languages}  >> ${pathToData}
 
-echo
-echo "[Client] Performance measurements..."
+for i in `ls $1`; do
+	bash plotResults.sh -p $1/$i
+	for j in `ls $1/$i`; do
+		if [ "$j" == "graph_data" ]; then
+			for scenario in ${ipc}; do
+				for energy in ${languages}; do
+					tmp=""
+					tmp=$(grep -w ${energy} $1/${i}/${j}/energy_server.txt | grep -w ${scenario} | awk -F":" '{print $NF}')
 
-for i in "${PROTOCOLS_ARRAY[@]}"; do
-        for j in "${LANGUAGES_ARRAY[@]}"; do
-                for k in `ls $1`; do
-                        VALUE=""
-                        PATH_DATA=$(echo "$1/$k/graph_data/performance_client.txt")
-
-
-                                VALUE=$(cat ${PATH_DATA} | grep "$i" | grep "$j" | awk -F ":" '{print $4}')
-
-                                if [ -z "${VALUE}" ]; then
-                                        break
-				fi
-			STRING=${STRING}","${VALUE}			
-		done
- 		echo "Results for $j and $i is ${STRING}" >> ../statistics/client_performance_values.txt
-		STRING=""
- 	done
+					if [ "${tmp}" == "" ]; then
+						tmp='0'
+					fi
+					results=$(echo "${results}	${tmp}")
+				done
+				echo ${results} >> ${pathToData} 
+				results=""
+			done
+		fi
+	done
 done
 
-echo
-echo "[Client] Energy measurements..."
-
-for i in "${PROTOCOLS_ARRAY[@]}"; do
-        for j in "${LANGUAGES_ARRAY[@]}"; do
-                for k in `ls $1`; do
-                        VALUE=""
-                        PATH_DATA=$(echo "$1/$k/graph_data/energy_client.txt")
-
-
-                                VALUE=$(cat ${PATH_DATA} | grep "$i" | grep "$j" | awk -F ":" '{print $4}')
-
-                                if [ -z "${VALUE}" ]; then
-                                        break
-				fi
-			STRING=${STRING}","${VALUE}			
-		done
- 		echo "Results for $j and $i is ${STRING}" >> ../statistics/client_energy_values.txt
-		STRING=""
- 	done
-done
-
-echo
-echo "[Server] Energy measurements..."
-
-for i in "${PROTOCOLS_ARRAY[@]}"; do
-        for j in "${LANGUAGES_ARRAY[@]}"; do
-                for k in `ls $1`; do
-                        VALUE=""
-                        PATH_DATA=$(echo "$1/$k/graph_data/energy_server.txt")
-
-
-                                VALUE=$(cat ${PATH_DATA} | grep "$i" | grep "$j" | awk -F ":" '{print $4}')
-
-                                if [ -z "${VALUE}" ]; then
-                                        break
-				fi
-			STRING=${STRING}","${VALUE}			
-		done
- 		echo "Results for $j and $i is ${STRING}" >> ../statistics/server_energy_values.txt
-		STRING=""
- 	done
-done
-
-
-
-echo
-echo "[Server] Performance measurements..."
-
-for i in "${PROTOCOLS_ARRAY[@]}"; do
-        for j in "${LANGUAGES_ARRAY[@]}"; do
-                for k in `ls $1`; do
-                        VALUE=""
-                        PATH_DATA=$(echo "$1/$k/graph_data/performance_server.txt")
-
-
-                                VALUE=$(cat ${PATH_DATA} | grep "$i" | grep "$j" | awk -F ":" '{print $4}')
-
-                                if [ -z "${VALUE}" ]; then
-                                        break
-				fi
-			STRING=${STRING}","${VALUE}			
-		done
- 		echo "Results for $j and $i is ${STRING}" >> ../statistics/server_performance_values.txt
-		STRING=""
- 	done
-done
+exit
