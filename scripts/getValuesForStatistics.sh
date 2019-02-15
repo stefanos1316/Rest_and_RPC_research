580fe8ab1c1f0f1b6cd691ac2aab2ba78df04e9e
stefanos1316 <stefanos1316@gmail.com>
2019-02-15 16:14:55 +0200

    Adding more scritps

--- scripts/getValuesForStatistics.sh
+++ scripts/getValuesForStatistics.sh
@@ -5,24 +5,27 @@
 
 # The $2 is to which filename to write the data
 
-pathToData=$2
 results=""
 languages="csharp	go	java	javascript	php	python	ruby"
 ipc="grpc rest rpc"
+files=$2
 
-if [ -f ${pathToData} ]; then
+if [ ${#files[@]} -gt 0 ]; then
    	echo "File already exist..."
 	echo Deleting
-	rm ${pathToData}
+	rm -rf ${files}/*
 fi
-
-echo ${languages}  >> ${pathToData}
 
 for i in `ls $1`; do
 	bash plotResults.sh -p $1/$i
+
 	for j in `ls $1/$i`; do
 		if [ "$j" == "graph_data" ]; then
 			for scenario in ${ipc}; do
+				pathToData=$(echo $2_${scenario})
+				echo ${languages}  >> ${pathToData}
+
+				
 				for energy in ${languages}; do
 					tmp=""
 					tmp=$(grep -w ${energy} $1/${i}/${j}/energy_server.txt | grep -w ${scenario} | awk -F":" '{print $NF}')
@@ -38,5 +41,11 @@ for i in `ls $1`; do
 		fi
 	done
 done
+
+echo
+echo "Obtained values from $(ls $1 | wc -l) executions..."
+echo "Calculating average values..."
+
+
 
 exit
