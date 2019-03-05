8404e2a5f129d3dda29ec240e5c659b87f2b652f
stefanos1316 <stefanos1316@gmail.com>
2019-03-05 23:58:34 +0200

    Got all describe values from platforms

--- scripts/getValuesForStatistics.sh
+++ scripts/getValuesForStatistics.sh
@@ -16,21 +16,14 @@ if [ ${#files[@]} -gt 0 ]; then
 	rm -rf ${files}/*.txt
 fi
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 22a6fc71f0a2ba638e7d0b76a19b4f719ae0c1b7
 for scenario in ${ipc}; do
 	pathToData=$(echo $2/${scenario}.txt)
 	echo ${languages}  >> ${pathToData}
 	for i in `ls $1`; do
-<<<<<<< HEAD
 		if [ ! -d $1/$i/graph_data ]; then
 			bash plotResults.sh -p $1/$i
 		fi
-=======
 		bash plotResults.sh -p $1/$i
->>>>>>> 22a6fc71f0a2ba638e7d0b76a19b4f719ae0c1b7
 
 		for j in `ls $1/$i`; do
 			if [ "$j" == "graph_data" ]; then
@@ -52,18 +45,9 @@ for scenario in ${ipc}; do
 
 					results=$(echo "${results}	${tmp}")
 				done
-<<<<<<< HEAD
 				echo ${results} >> ${pathToData} 
-			
-				echo $i"  "${results}
 				results=""
 			fi
-=======
-				#echo "${i}	${results}" >> ${pathToData} 
-				echo "${results}" >> ${pathToData}
-				results=""
-			fi	
->>>>>>> 22a6fc71f0a2ba638e7d0b76a19b4f719ae0c1b7
 		done
 	done
 done
